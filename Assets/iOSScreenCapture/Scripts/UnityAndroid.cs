2d2629eedbd74830c6a75ac30a3f0e11efd5d75c
rihoishiyama <riho.1112.mj@gmail.com>
2018-07-21 00:47:10 +0900

    change UI and add sound of shutter

--- Assets/iOSScreenCapture/Scripts/UnityAndroid.cs
+++ Assets/iOSScreenCapture/Scripts/UnityAndroid.cs
@@ -6,35 +6,32 @@ using System.IO;
 
 public class UnityAndroid : MonoBehaviour
 {
-
-    //ここから
     [SerializeField] private GameObject canvas;
-    [SerializeField] private Text testText;
     private string imageName = "";
 
 	public void ScreenCapture_Android()
     {
+        //写真をとる瞬間だけUI非表示にする
         canvas.SetActive(false);
         StartCoroutine("Captcha");
     }
 
     IEnumerator Captcha() {
 
-        //ファイル名 ※カスタムDateTimeじゃないとファイル名に/と:が入って保存失敗する
+        //画像名
         imageName = System.DateTime.Now.ToString ("gyyyyMMddtthhmmssfff") + ".png";
 
         //ios,Android時パスを追加
 #if !UNITY_EDITOR && UNITY_ANDROID && !UNITY_IOS
         Scan(imageName);
-        //testText.text = imageName;
 #endif
         yield return new WaitForEndOfFrame();
-        //スクリーンショット
+
+        //スクリーンショットをとる
         ScreenCapture.CaptureScreenshot(imageName);
         canvas.SetActive(true);
 
 #if !UNITY_EDITOR && UNITY_ANDROID && !UNITY_IOS
-        //↑モバイルではうまく動かないようなのでFileStreamで保存する
         var tex = new Texture2D (Screen.width, Screen.height, TextureFormat.RGB24, false);
         tex.ReadPixels (new Rect (0, 0, Screen.width, Screen.height), 0, 0);
         using (FileStream BinaryFile = new FileStream (imageName, FileMode.Create, FileAccess.Write)) 
@@ -46,7 +43,6 @@ public class UnityAndroid : MonoBehaviour
         }
 
 #endif
-        //待つ
         yield return new WaitForEndOfFrame ();
         //保存まで待機
         float latency = 0, latencyLimit=2;
@@ -59,12 +55,8 @@ public class UnityAndroid : MonoBehaviour
             yield return null;
         }
 
+        //メディアスキャンをする
         ScanMedia (imageName);
-
-        //待機時間を超えた
-        if (latency >= latencyLimit) {
-            testText.text = "ファイル保存失敗";
-        }
 
     }
 
@@ -87,6 +79,7 @@ public class UnityAndroid : MonoBehaviour
 #endif
     }
 
+    //Android端末内のディレクトリを読み込んで、オリジナル保存フォルダを生成する
     void Scan(string fileName)
     {
 #if !UNITY_EDITOR && UNITY_ANDROID && !UNITY_IOS
@@ -100,17 +93,11 @@ public class UnityAndroid : MonoBehaviour
         string imageNameDirectory = joExDir.Call<string>("toString") + "/DCIM/Unipro/";
             if (!Directory.Exists(imageNameDirectory))
             {
-                testText.text = "search path2 : " + imageNameDirectory;
                 System.IO.Directory.CreateDirectory(imageNameDirectory);
-                testText.text = "search path3 : " + imageNameDirectory;
                 return;
             }
             imageName = joExDir.Call<string>("toString") + "/DCIM/Unipro/" + fileName;
-            //testText.text = "search path : " + imageName;
         }
 #endif   
     }
-
-
-
 }
